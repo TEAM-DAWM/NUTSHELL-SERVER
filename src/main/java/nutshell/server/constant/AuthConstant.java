6795252c73e3a0524a3e825402025b64e0f16172
minwoo0419 <ghdl464@naver.com>
2024-06-30 20:10:43 +0900

    :zap: [feat] #3 AuthConstant 변수 추가

--- src/main/java/nutshell/server/constant/AuthConstant.java
+++ src/main/java/nutshell/server/constant/AuthConstant.java
@@ -4,6 +4,7 @@ public class AuthConstant {
     public static final String USER_ID_CLAIM_NAME = "uid";
     public static final String BEARER_PREFIX = "Bearer ";
     public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String NO_AUTHORIZATION_USER = "anonymousUser";
     public static final String[] AUTH_WHITELIST = {
             "/api/**"
     };
