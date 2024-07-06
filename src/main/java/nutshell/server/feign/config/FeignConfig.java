45d0c2bf1ae449a395071f198332f18bcabf778a
choyeongju <jjudy1301@naver.com>
2024-07-07 02:33:57 +0900

    :heavy_plus_sign: [chore] Feign 설정 파일 추가

--- src/main/java/nutshell/server/feign/config/FeignConfig.java
+++ src/main/java/nutshell/server/feign/config/FeignConfig.java
@@ -4,7 +4,7 @@ import nutshell.server.ServerApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @EnableFeignClients(basePackageClasses = ServerApplication.class)
+@Configuration
 public class FeignConfig {
 }
