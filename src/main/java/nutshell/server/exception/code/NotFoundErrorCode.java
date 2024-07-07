8d18099a2bc13e97da0929004a7cf632d4e85f75
Minwoo Cho <103352114+minwoo0419@users.noreply.github.com>
2024-07-08 01:29:26 +0900

    [hotfix] conflict 에러 수정

--- src/main/java/nutshell/server/exception/code/NotFoundErrorCode.java
+++ src/main/java/nutshell/server/exception/code/NotFoundErrorCode.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum NotFoundErrorCode implements DefaultErrorCode{
     NOT_FOUND_END_POINT(HttpStatus.NOT_FOUND, "error", "존재하지 않는 API입니다."),
     NOT_FOUND_TASK(HttpStatus.NOT_FOUND, "error", "존재하지 않는 Task 입니다."),
-    NOT_FOUND_USER(HttpStatus.NOT_FOUND,"error","존재하지 않는 사용자 입니다.")
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND,"error","존재하지 않는 사용자 입니다."),
     NOT_FOUND_REFRESH_TOKEN(HttpStatus.NOT_FOUND, "error","리프레시 토큰을 찾을 수 없습니다."),
     ;
 
