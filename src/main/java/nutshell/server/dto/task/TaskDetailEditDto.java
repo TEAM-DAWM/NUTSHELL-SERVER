8844d0788b9b6edc93ab44818e1a058abaabe970
choyeongju <jjudy1301@naver.com>
2024-07-09 22:42:17 +0900

    :sparkles: # 34 [feat] Task에 시작시간, 마감시간 dto에서 삭제

--- src/main/java/nutshell/server/dto/task/TaskDetailEditDto.java
+++ src/main/java/nutshell/server/dto/task/TaskDetailEditDto.java
@@ -7,10 +7,6 @@ import java.time.LocalDateTime;
 public record TaskDetailEditDto(
         String name,
         String description,
-        TaskCreateDto.DeadLine deadLine,
-        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
-        LocalDateTime startTime,
-        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
-        LocalDateTime endTime
+        TaskCreateDto.DeadLine deadLine
 ) {
 }
