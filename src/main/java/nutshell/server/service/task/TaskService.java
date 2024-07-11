f4c246fbe4a6bc70dfadc1cc7dcea2443c08bade
choyeongju <jjudy1301@naver.com>
2024-07-11 20:50:35 +0900

    :sparkles: #58 [feat] Task 삭제 DELETE API 구현 완료

--- src/main/java/nutshell/server/service/task/TaskService.java
+++ src/main/java/nutshell/server/service/task/TaskService.java
@@ -1,9 +1,23 @@
 package nutshell.server.service.task;
 
 import lombok.RequiredArgsConstructor;
+import nutshell.server.domain.Task;
+import nutshell.server.domain.User;
+import nutshell.server.repository.TaskRepository;
+import nutshell.server.service.user.UserRetriever;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class TaskService {
+
+    private final TaskRetriever taskRetriever;
+    private final UserRetriever userRetriever;
+    private final TaskRemover taskRemover;
+
+    public void removeTask(final Long userId, final Long taskId){
+        User user= userRetriever.findByUserId(userId);
+        Task task = taskRetriever.findByUserAndId(user, taskId);
+        taskRemover.deleteTask(task);
+    }
 }
