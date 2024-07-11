cd1c0eb3e70da69c334d45e64b3833886ac7dc82
minwoo0419 <ghdl464@naver.com>
2024-07-11 22:29:00 +0900

    :zap: [feat] #55 TaskStatusRepository 주석 추가

--- src/main/java/nutshell/server/repository/TaskStatusRepository.java
+++ src/main/java/nutshell/server/repository/TaskStatusRepository.java
@@ -10,20 +10,25 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TaskStatusRepository extends JpaRepository<TaskStatus, Long> {
+    //Task를 상속 받고 있고 targetDate보다 미래인 것들 중 Deferred 상태가 아닌 것들을 찾아 반환
     List<TaskStatus> findAllByTaskAndTargetDateGreaterThanAndStatusNot(
             final Task task,
             final LocalDate targetDate,
             final Status status
     );
+    //Task를 상속 받고 있고 해당 targetDate를 가지고 있으며 Deferred 상태가 아닌 것이 있는지 반환
     Boolean existsByTaskAndTargetDateAndStatusNot(
             final Task task,
             final LocalDate targetDate,
             final Status status
     );
+    //Task를 상속 받고 있고 해당 targetDate를 가지고 있으며 Deferred 상태가 아닌 것을 찾아 반환
     Optional<TaskStatus> findByTaskAndTargetDateAndStatusNot(final Task task, final LocalDate targetDate, final Status status);
 
+    //Task를 상속 받고 있고 해당 targetDate를 가지고 있는 것들을 반환
     List<TaskStatus> findAllByTargetDate(final LocalDate TargetDate);
 
+    //Task를 상속 받고 있고 해당 targetDate가 아니며 Deferred 상태가 아닌 것들을 찾아 반환
     List<TaskStatus> findAllByTaskAndStatusNotAndTargetDateNot(final Task task, final Status status, final LocalDate targetDate);
 
 
