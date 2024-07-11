1720e63e49dfd38837fa8531d2111523d591cf7b
minwoo0419 <ghdl464@naver.com>
2024-07-12 00:32:16 +0900

    :zap: [feat] #62 task List 조회 API 구현

---
+++ src/main/java/nutshell/server/dto/task/TasksDto.java
@@ -0,0 +1,19 @@
+package nutshell.server.dto.task;
+
+import lombok.Builder;
+
+import java.util.List;
+@Builder
+public record TasksDto(
+        List<TaskItemDto> tasks
+) {
+    @Builder
+    public record TaskItemDto(
+            Long id,
+            String name,
+            TaskCreateDto.DeadLine deadLine,
+            Boolean hasDescription,
+            String status
+    ) {
+    }
+}
--- src/main/java/nutshell/server/repository/TaskRepository.java
+++ src/main/java/nutshell/server/repository/TaskRepository.java
@@ -3,9 +3,31 @@ package nutshell.server.repository;
 import nutshell.server.domain.Task;
 import nutshell.server.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Optional<Task> findByUserAndId(User user, Long id);
+
+    List<Task> findAllByUserAndAssignedDateIsNullOrderByCreatedAtDesc(final User user);
+
+    List<Task> findAllByUserAndAssignedDateIsNullOrderByCreatedAtAsc(final User user);
+
+    @Query(
+            value = "select * from task t where t.user_id = :userId " +
+                    "AND t.assigned_date is null " +
+                    "order by abs(extract(epoch from now() - t.dead_line)) asc nulls last"
+            , nativeQuery = true
+    )
+    List<Task> findAllByUserAndAssignedDateIsNullOrderByTimeDiffAsc(final Long userId);
+
+    @Query(
+            value = "select * from task t where t.user_id = :userId " +
+                    "AND t.assigned_date is null " +
+                    "order by abs(extract(epoch from now() - t.dead_line)) desc nulls last"
+            , nativeQuery = true
+    )
+    List<Task> findAllByUserAndAssignedDateIsNullOrderByTimeDiffDesc(final Long userId);
 }
