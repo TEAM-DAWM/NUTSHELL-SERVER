590853a1b46d5e9bba161023d22e2f7e38249bb4
choyeongju <jjudy1301@naver.com>
2024-07-13 21:04:05 +0900

    :recycle: [feat] #78 안쓰는 에러코드 삭제 및 문맥 통일

--- src/main/java/nutshell/server/exception/code/ForbiddenErrorCode.java
+++ src/main/java/nutshell/server/exception/code/ForbiddenErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ForbiddenErrorCode implements DefaultErrorCode{
-
+    //인증된 클라이언트가 권한이 없는 자원에 접근할 때 응답하는 상태 코드
+    // 근데 우리 프로젝트에는 이게 필요한가??
     FORBIDDEN(HttpStatus.FORBIDDEN, "error", "접근 권한이 없습니다."),
     ;
 
