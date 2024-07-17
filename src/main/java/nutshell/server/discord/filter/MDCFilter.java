de71142c65f6dc0c0458dc750c7dfdfef22f30c2
choyeongju <jjudy1301@naver.com>
2024-07-17 19:50:58 +0900

    :sparkles: [feat] #87 로깅에서 필요한 MDC 구현

---
+++ src/main/java/nutshell/server/discord/filter/MDCFilter.java
@@ -0,0 +1,39 @@
+package nutshell.server.discord.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import nutshell.server.discord.util.HttpRequestUtil;
+import nutshell.server.discord.util.MDCUtil;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class MDCFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        HttpServletRequest httpReq = WebUtils.getNativeRequest(request, HttpServletRequest.class);
+
+        MDCUtil.setJsonValue(MDCUtil.REQUEST_URI_MDC, HttpRequestUtil.getRequestUri(Objects.requireNonNull(httpReq)));
+        MDCUtil.setJsonValue(MDCUtil.USER_IP_MDC, HttpRequestUtil.getUserIP(Objects.requireNonNull(httpReq)));
+        MDCUtil.setJsonValue(MDCUtil.HEADER_MAP_MDC, HttpRequestUtil.getHeaderMap(httpReq));
+        MDCUtil.setJsonValue(MDCUtil.USER_REQUEST_COOKIES, HttpRequestUtil.getUserCookies(httpReq));
+        MDCUtil.setJsonValue(MDCUtil.PARAMETER_MAP_MDC, HttpRequestUtil.getParamMap(httpReq));
+        MDCUtil.setJsonValue(MDCUtil.BODY_MDC, HttpRequestUtil.getBody(httpReq));
+
+        filterChain.doFilter(request, response);
+
+    }
+}
